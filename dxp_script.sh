61d335d068f608f8e7cc1a38c071e843b5fd2c58
DizzyHurricane5001 <72887223+DizzyHurricane5001@users.noreply.github.com>
2021-06-24 13:58:11 +0100

    Update dxp_script.sh

--- dxp_script.sh
+++ dxp_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # @Tom B
 # Made for use with bootstrap tables
-filename='tt_server_addresses.txt'
+TTAddr='tt_server_addresses.txt'
 cp index_template.html index.html
 while read addr; do 
     echo \<tr\> \<td\> "$addr" \<\/td\>\<td\> >> index.html
     wget -q -t 1 --timeout=5 --header="X-Tycoon-Key:X-Tycoon-Key" $addr -O -| jq -r '.server.dxp' | sed 's|[],[/n]||g' >> index.html # OR grep -Po '"dxp":.*?[^\\]"' >> index.html (modify sed)
     echo \<\/td\>\<\/tr\> >> index.html
-done < $filename
+done < $TTAddr
 
 # tt_server_addresses.txt example:
 #http://server.tycoon.community:30122/status/widget/players.json
